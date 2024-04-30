97cb8b2baea292f44bc453c174673a559814a02f
AS <sagaradavian@gmail.com>
2024-04-30 15:07:07 +0530

    Authentication part done

---
+++ src/test/java/org/example/userservice/UserserviceApplicationTests.java
@@ -0,0 +1,43 @@
+package org.example.userservice;
+
+import org.example.userservice.security.repository.JpaRegisteredClientRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.core.oidc.OidcScopes;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
+import org.springframework.test.annotation.Commit;
+
+import java.util.UUID;
+
+@SpringBootTest
+class UserserviceApplicationTests {
+    @Autowired
+    private JpaRegisteredClientRepository registeredClientRepository;
+
+    @Test
+    void contextLoads() {
+    }
+    @Test
+    @Commit
+    void storeRegisteredClientIntoDb(){
+        RegisteredClient oidcClient = RegisteredClient.withId(UUID.randomUUID().toString())
+                .clientId("oidc-client")
+                .clientSecret("{noop}secret")
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+                .redirectUri("https://oauth.pstmn.io/v1/callback")
+                .postLogoutRedirectUri("https://oauth.pstmn.io/v1/callback")
+                .scope(OidcScopes.OPENID)
+                .scope(OidcScopes.PROFILE)
+                .scope("ADMIN")
+                .clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build())
+                .build();
+        registeredClientRepository.save(oidcClient);
+    }
+
+}
